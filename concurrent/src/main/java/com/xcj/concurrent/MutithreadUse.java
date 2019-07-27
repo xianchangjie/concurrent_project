838066b20caa118ed3aa6f6071610c646b538a3a
changjiexian <changjie.xian@hand-china.com>
2019-07-28 00:01:03 +0800

    Java多线程技能实践

---
+++ concurrent/src/main/java/com/xcj/concurrent/MutithreadUse.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2019. changjie.xian All Rights Reserved
+ *
+ */
+
+package com.xcj.concurrent;
+
+/**
+ * @author changjiexian
+ */
+public class MutithreadUse {
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName());
+    }
+}
